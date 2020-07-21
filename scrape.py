159401453e2d845b8eff5e754419d8dcf2a8dc1f
hmaag <maag.heath@gmail.com>
2020-07-20 22:42:42 -0700

    Hacky script in place - need to refine

--- scrape.py
+++ scrape.py
@@ -8,7 +8,7 @@ import time
 # Constants
 DRIVER_PATH = "/usr/bin/chromedriver"
 BASE_DIR = os.path.dirname(__file__)
-INIT_URL = "https://www.huskermax.com/statistics/2019-nebraska-football-statistics/"
+INIT_URL = "https://www.huskermax.com/statistics/1969-nebraska-football-statistics/"
 
 # Make data folder
 DATA_FOLDER = os.path.join(BASE_DIR, "data")
@@ -17,11 +17,11 @@ os.makedirs(DATA_FOLDER, exist_ok=True)
 # PDF - /html/body/div[3]/main/article/div[1]/div[3]/iframe
 # Text - /html/body/div[3]/main/article/div[1]/div[2]/iframe
 
-def change_year(driver, frame_idx, decade_idx, year_idx):
+def change_year(driver, body_div_idx, frame_idx, decade_idx, year_idx):
     # action chains to provide mouse movement/events
     action = ActionChains(driver)
     # dropdown is located within this iframe --> need to switch to it and perform actions within it
-    iframe = driver.find_element_by_xpath(f"/html/body/div[3]/main/article/div[1]/div[{frame_idx}]/iframe") # frame location
+    iframe = driver.find_element_by_xpath(f"/html/body/div[{body_div_idx}]/main/article/div[1]/div[{frame_idx}]/iframe") # frame location
     driver.switch_to.frame(iframe)
     # navigate to menu -> submenus and click
     firstLevelMenu = driver.find_element_by_xpath("//*[@id=\"navHmx\"]") # main menu
@@ -40,17 +40,23 @@ def get_data(driver, year):
 
     if len(raw_data) == 0: #aka - data is hosted in PDF
         with open(pdfpath, 'a') as file:
-            file.write(f"{year}") # this was "2013"
+            file.write(f"{year}\n")
         return False
     else:
         with open(filepath, 'a') as file: #success - data is accessible
             for data in raw_data:
-                if data == raw_data[2]: #we skip index 2 because it's a duplicate (for some weird reason)
-                    continue
                 file.write(data.get_attribute("textContent")) # have to use .get_attribute("textContent") instead of .text because raw_data[3] is hidden
         return True
 
 # driver.switch_to.default_content() # switch back to default content WILL WE NEED THIS?
+
+def find_dropdown(driver, body_div_idx, frame_idx):
+    try:
+        iframe = driver.find_element_by_xpath(f"/html/body/div[{body_div_idx}]/main/article/div[1]/div[{frame_idx}]/iframe")
+        return True
+    except:
+        return False
+        
 
 def run(year, decade_idx, year_idx):
     driver = webdriver.Chrome(DRIVER_PATH)
@@ -61,7 +67,6 @@ def run(year, decade_idx, year_idx):
     
     while year >= 1962:
         data_status = get_data(driver, year)
-        year -= 1
         year_idx += 1
         if year_idx == 11:
             decade_idx += 1
@@ -70,13 +75,19 @@ def run(year, decade_idx, year_idx):
             frame_idx = 3
         else:
             frame_idx = 2
-        change_year(driver, frame_idx, decade_idx, year_idx)
+        body_div_idx = 3
+        no_add = find_dropdown(driver, body_div_idx, frame_idx)
+        if not no_add:
+            body_div_idx = 4
+
+        change_year(driver, body_div_idx, frame_idx, decade_idx, year_idx)
+        year -= 1
         time.sleep(5)
 
 if __name__ == "__main__":
     # Initializations
-    year = 2019 # start from 2019 and work back to 1962
-    decade_idx = 1 # index of 2010s
+    year = 1969 # start from 2019 and work back to 1962
+    decade_idx = 6 # index of 2010s
     year_idx = 1 # top year of decade, e.g. index of 2019
     run(year, decade_idx, year_idx)
     driver.quit()
